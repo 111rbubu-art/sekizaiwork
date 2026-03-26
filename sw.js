fc4102fa502c824be02f0851f012f5171713317f
111rbubu-art <111rbubu@gmail.com>
2026-03-26 09:23:59 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-var CACHE = 'sekizai-v1';
+var CACHE = 'sekizai-v3';
 var FILES = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', function(e) {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.filter(function(n) { return n !== CACHE; }).map(function(n) { return caches.delete(n); }));
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {

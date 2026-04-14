acc0d6ea08db0df686267c2692d6fc5b5aef4080
111rbubu-art <111rbubu@gmail.com>
2026-04-14 20:54:41 +0900

    Create map_config.js

---
+++ map_config.js
@@ -0,0 +1,10 @@
+// map_config.js - オフラインマップ設定ファイル
+// このファイルを編集してオフライン動作を設定してください
+
+// 表示するお寺（マップ）の選択
+// 対応値: 'jikoji'（慈宏寺）, 'chouzenji'（長泉寺）など
+var OFFLINE_MODE_TEMPLE = 'jikoji';
+
+// デバッグ用：オンライン環境でもオフラインモードを強制する場合は true に変更
+// 通常は false のままにしてください
+var FORCE_OFFLINE = false;
